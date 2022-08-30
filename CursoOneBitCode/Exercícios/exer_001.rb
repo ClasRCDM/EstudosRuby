6833a9815510df5c6352292d8300f0f0ae2772c3
Clas <raphaelcalixto2013@gmail.com>
2022-08-30 08:40:15 -0400

    Escreva o nome completo e printa, exer 1

---
+++ CursoOneBitCode/Exercícios/exer_001.rb
@@ -0,0 +1,11 @@
+# Nome completo
+# Nome
+print 'Qual é o seu primeiro nome? '
+nome = gets.chomp
+
+# Sobrenome
+print 'Qual é o seu sobrenome? '
+sobrenome = gets.chomp
+
+# Result
+puts "Olá, Bom dia!! #{nome} #{sobrenome}, turo bem?"
