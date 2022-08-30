dca1c78694a263ddba8cf1b3338ffc4828385072
Clas <raphaelcalixto2013@gmail.com>
2022-08-30 08:46:12 -0400

    Contas, exer 2

---
+++ CursoOneBitCode/Exercícios/exer_002.rb
@@ -0,0 +1,13 @@
+# Soma, Subtração e divisão
+# Primeiro número
+print 'Primeiro número? '
+num1 = gets.chomp.to_i
+
+# Segundo número
+print 'Segundo número? '
+num2 = gets.chomp.to_i
+
+# Result
+puts "A soma entre #{num1} e #{num2} é #{num1+num2}."
+puts "A subtração entre #{num1} e #{num2} é #{num1-num2}."
+puts "A divisão entre #{num1} e #{num2} é #{num1/num2}."
