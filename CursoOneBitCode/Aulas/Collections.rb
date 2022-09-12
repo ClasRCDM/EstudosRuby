35ca03c8e1d0dee3ca44ae80a62f1ee798d0a33b
Clas_RCDM <raphaelcalixto2013@gmail.com>
2022-09-12 09:48:39 -0400

    Collections / Array

---
+++ CursoOneBitCode/Aulas/Collections.rb
@@ -0,0 +1,45 @@
+# Collections e suas manipulações
+
+estados = []  # Lista em python
+
+estados.push('Mato Grosso')
+estados.push('Mato Grosso do Sul')
+
+print estados
+
+estados.insert(0, 'Brasilia')
+
+puts ''  # Space
+
+print estados
+
+puts ''  # Space
+
+puts estados.first  # Primeiro elemento
+puts estados.last  # Ultimo elemento
+
+puts "O array tem ao todo #{estados.count} itens"
+
+puts "O array esta vazio? #{estados.empty?}"  # Se estiver fazio é true, se não é false
+
+if estados.include?('Mato Grosso')
+    puts 'MatoGrosso esta no array.'
+end
+if not estados.include?('São Paulo')
+    puts 'São paulo não esta no array.'
+end
+
+estados.delete_at(0)  # Deleta o index escolhido
+
+print estados
+puts ''  # Space
+
+estados.pop()  # Deleta o ultimo
+
+print estados
+puts ''  # Space
+
+estados.shift()  # Deleta o primeiro
+
+print estados
+puts ''  # Space
