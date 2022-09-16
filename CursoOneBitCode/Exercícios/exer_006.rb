f002b14e20ef6f79550b10abb8a60b6d10f891c5
Clas_RCDM <raphaelcalixto2013@gmail.com>
2022-09-15 20:01:27 -0400

    Resolução de expoente, exer 7

--- CursoOneBitCode/Exercícios/exer_006.rb
+++ CursoOneBitCode/Exercícios/exer_006.rb
@@ -1,6 +1,8 @@
 # Selecionando o maior
 
+# Números
 NUMBERS = {a: 10, B: 30, C: 20, D: 25, E: 15}
 
+# result
 C, V = NUMBERS.min
 puts "Item #{C}: Valor #{V}"
---
+++ CursoOneBitCode/Exercícios/exer_007.rb
@@ -0,0 +1,16 @@
+# Resolvendo Expoente
+
+# hash
+hash = Hash.new
+
+# Nome das classes
+puts "\t>> Potência... <<"
+
+print 'Número base: '
+num1 = gets.chomp.to_i
+
+print 'Seu expoente: '
+num2 = gets.chomp.to_i
+
+# result
+puts "A resolução da potência de #{num1} e #{num2} :> #{num1**num2}"
